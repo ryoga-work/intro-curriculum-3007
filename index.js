50469a3f445a7ffe5464dc90cd0af64e814f964c
ryoga-work <kubota.ryoga.work@gmail.com>
2024-05-02 08:49:24 +0900

    17の実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
+/**
+ * 17の倍数である場合trueを返す
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num){
+  if(num%17===0){
+    return true;
+  }
+  return false;
+}
 
-module.exports = {
+module.exports = {isMultipleOfSeventeen
 }
